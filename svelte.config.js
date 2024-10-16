ea3603db74aa5ed0426fd2469991b1c1b03c11b0
z3n01d <madarik.sim@gmail.com>
2024-10-16 19:18:58 +0200

    Svelte stuff for Github pages

--- svelte.config.js
+++ svelte.config.js
@@ -4,4 +4,11 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+
+  kit: {
+      adapter: adapter(),
+      paths: {
+          base: process.env.NODE_ENV === 'production' ? '/z3n01d.github.io' : '',
+      }
+  }
 }
